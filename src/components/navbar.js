b75c0ed45ee3a19641ea3160893817ccdb19f87e
Ajit Singh <ajitscorpio@gmail.com>
2019-06-13 23:53:46 -0400

    First Commit with base structure

---
+++ src/components/navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Menu, Icon } from 'semantic-ui-react'
+
+const Navbar = ({selectedSub, handleMenuChange, handleRefreshClick}) => (
+    <div style={{ margin: '1rem' }}>
+        Reddit Client
+    </div>
+)
+
+export default Navbar
